70e4b29f5ca619a113f155876980ba78fcb0f36f
AntonPDC <antonpdecesare@gmail.com>
2026-08-21 10:39:28 -0400

    values1

--- annotations.ts
+++ annotations.ts
@@ -2,3 +2,14 @@ let apples : number = 5;
 apples = 10;
 
 let speed : string = "fast";
+
+
+let haveName : boolean = true;
+
+let nothingMuch : null = null;
+
+let nothing : undefined = undefined;
+
+
+//Built in objects
+let now : Date = new Date();
