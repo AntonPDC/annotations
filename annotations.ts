3b88f403e3ab65bfafc93c40f213fb669909399e
AntonPDC <antonpdecesare@gmail.com>
2026-08-24 13:05:23 -0400

    stillAnnotations

--- annotations.ts
+++ annotations.ts
@@ -1,43 +1,48 @@
-let apples : number = 5;
+let apples: number = 5;
 apples = 10;
 
-let speed : string = "fast";
+let speed: string = "fast";
 
+let haveName: boolean = true;
 
-let haveName : boolean = true;
+let nothingMuch: null = null;
 
-let nothingMuch : null = null;
-
-let nothing : undefined = undefined;
-
+let nothing: undefined = undefined;
 
 //Built in objects
-let now : Date = new Date();
-
+let now: Date = new Date();
 
 // Arrays
-let colors : string[] = ["red", "green", "blue"];
-let myNumbers : number[] = [1 ,2, 3, 4];
-let truths : boolean[] = [ true, true, false]
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3, 4];
+let truths: boolean[] = [true, true, false];
 
 // Classes
-class Car {
+class Car {}
 
-}
-
-let car : Car = new Car();
-
+let car: Car = new Car();
 
 // Object Literals
-let point: {x : number, y : number} = {
-    x: 10,
-    y: 20
-}
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
 
 //Functions
-const LogNumber: (i : number) => void = (i: number) => {
-    console.log(i);
-}
+const LogNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
 
 // When to use annotations
 //1) Function returns the "any" type
+const json = '{"x": 10, "y": 20}';
+const coordinates : {x : number, y : number}= JSON.parse(json);
+
+
+//2) When we declare a variable on one line and initialize it later
+let words : string[] = ["red", "green", "blue"];
+let foundWord;
+
+for (let i = 0, i < words.length; i++){
+
+}
